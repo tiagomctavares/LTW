83921acfca727fd3f6470ab46acaa82730a15c4d
Tiago Tavares <tiagomctavares@gmail.com>
2014-11-12 03:14:22 +0000

    Changed some comments

--- project/resources/lib/templateFunctions.php
+++ project/resources/lib/templateFunctions.php
@@ -2,9 +2,7 @@
     function renderLayoutWithContentFile($contentFile, $variables = array())
     {
         $contentFileFullPath = TEMPLATES_PATH . "/pages/" . $contentFile;
-     
-        // making sure passed in variables are in scope of the template
-        // each key in the $variables array will become a variable
+    
         if (count($variables) > 0) {
             foreach ($variables as $key => $value) {
                 if (strlen($key) > 0) {
@@ -15,9 +13,6 @@
      
         require_once(TEMPLATES_PATH . "/header.php");
      
-        /*echo "<div id=\"container\">\n"
-           . "\t<div id=\"content\">\n";
-        */
         if (file_exists($contentFileFullPath)) {
             require_once($contentFileFullPath);
         } else {
@@ -27,13 +22,7 @@
             */
             require_once(TEMPLATES_PATH . "/error.php");
         }
-        /*
-        // close content div
-        echo "\t</div>\n";
-     
-        // close container div
-        echo "</div>\n";
-        */
+        
         require_once(TEMPLATES_PATH . "/footer.php");
     }
 ?>
