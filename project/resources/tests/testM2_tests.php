3df3796f331112808d0e0dc28d74b12b5b9c0bc9
Tiago Tavares <tiagomctavares@gmail.com>
2014-11-18 16:10:55 +0000

    Added folder for js and css and some fixes for milestone 2

---
+++ project/resources/tests/testM2_tests.php
@@ -0,0 +1,55 @@
+<?php
+require_once(VIEWS_PATH.'/user.php');
+
+class testM1_actions extends UnitTestCase {
+	function test_() {
+	    $_POST['username'] = '_ttavares';
+	    $_POST['password'] = '1234';
+	    $this->assertEqual(registerUser(), 1);
+	}
+
+	function test_login() {
+	    $_POST['username'] = '_ttavares';
+	    $_POST['password'] = '1234';
+	    $this->assertEqual(loginUser(), 1);
+	}
+
+	function test_registerDuplicate() {
+	    $_POST['username'] = '_ttavares';
+	    $_POST['password'] = '1234';
+	    $this->assertEqual(registerUser(), -10);
+	}
+
+	function test_registerErrorInput() {
+	    $_POST['username'] = 'ttttttttttttttttttttttttttttttttttttttttttttttttttt';
+	    $_POST['password'] = '1234';
+	    $this->assertEqual(registerUser(), -1);
+	}
+
+	function test_loginErrorInput() {
+	    $_POST['username'] = 'ttttttttttttttttttttttttttttttttttttttttttttttttttt';
+	    $_POST['password'] = '1234';
+	    $this->assertEqual(loginUser(), -1);
+	}
+
+	function test_loginBadCredentials() {
+	    $_POST['username'] = '_ttavares';
+	    $_POST['password'] = '12345';
+	    $this->assertEqual(loginUser(), -10);
+	}
+
+	function test_logout() {
+	    $this->assertEqual(logoutUser(), 1);
+	}
+
+	function test_logoutError() {
+	    $this->assertEqual(logoutUser(), -1);
+	}
+
+	function test_cleanDB() {
+		require_once(LIB_PATH.'/mypdo.php');
+		$pdo = new myPDO();
+		$pdo->query('DELETE FROM User WHERE username LIKE "_ttavares";');
+	}
+}
+?>
