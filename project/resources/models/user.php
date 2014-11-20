5c771ca52484efd6e8aeb50a801b6b44b94db501
Tiago Tavares <tiagomctavares@gmail.com>
2014-11-20 11:24:45 +0000

    Added user on models

---
+++ project/resources/models/user.php
@@ -0,0 +1,78 @@
+<?php
+require_once(LIB_PATH.'/mypdo.php');
+
+interface iUser {
+	/* Add User in Database
+	*  
+	* @param (array) with
+	* (text) username
+	* (text) password
+	* @ return (int) 
+	*/
+	function insertEntry($params);
+
+	/* Check Login Creditials of user in application 
+	* 
+	* @param (array) with
+	* (text) username
+	* (text) password
+	* @return (int) userId
+	*/
+	function correctCredentials($params);
+
+	/* Checks if username already exists
+	* 
+	* @param (text) username
+	* @return (bool)
+	*/
+	function existUsername($username);
+}
+# Model User
+class mUser implements iUser {
+	function insertEntry($params) {
+		$pdo = new myPDO();
+		$data[] = new myPDOparam($params[0], PDO::PARAM_STR);
+		//var_dump($data);
+		$data[] = new myPDOparam(hash('sha256', $params[1]), PDO::PARAM_STR);
+		//var_dump($data);
+		$data[] = new myPDOparam(@date("Y-m-d H:i:s"), PDO::PARAM_STR);
+		//var_dump($data);
+
+		$result = $pdo->query('INSERT INTO user (username, password, createDate) VALUES(?, ?, ?);', $data);
+
+		//var_dump($result);
+
+		return $result[0]==0;
+
+	}
+
+	function correctCredentials($params) {
+		$pdo = new myPDO();
+		$data[] = new myPDOparam($params[0], PDO::PARAM_STR);
+		$data[] = new myPDOparam(hash('sha256', $params[1]), PDO::PARAM_STR);
+		
+		$result = $pdo->query('SELECT id FROM user WHERE username LIKE ? AND password LIKE ?;', $data);
+		
+		$result = empty($result)?0:$result[0]->id;
+
+		if($result>0) {
+			# Reset Array
+			$data = array();
+			$data[] = new myPDOparam(@date("Y-m-d H:i:s"), PDO::PARAM_STR);
+			$data[] = new myPDOparam($result[0], PDO::PARAM_INT);
+			$pdo->query('UPDATE user SET lastLogin=? WHERE id=?', $data);
+		}
+
+		return $result;
+	}
+
+	function existUsername($username) {
+		$pdo = new myPDO();
+		$data[] = new myPDOparam($username, PDO::PARAM_STR);
+
+		$result = $pdo->query('SELECT COUNT(*) as res FROM user WHERE username LIKE ?;', $data);
+		
+		return ($result[0]->res)>0;
+	}
+}
+?>
