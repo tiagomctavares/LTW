1efde2b116b3a263124e67993b8369a640366371
Mário Macedo <andr.macedo1@gmail.com>
2014-11-20 23:07:26 +0000

    Changes maded on login and register modal. QRcodes added to footer

--- project/resources/templates/footer.php
+++ project/resources/templates/footer.php
@@ -2,10 +2,51 @@
 	<footer>
 		<div class="navbar navbar-default navbar-fixed-bottom">
 			<div class="container">
+				<a href="#" class="pull-right navbar-text" id="qrcode" data-toggle="modal" data-target="#students">
+					<i class="glyphicon glyphicon-qrcode"></i>
+				</a>
 				<p class="navbar-text pull-right"> Built by Tiago Tavares, Pedro Faria and Mário Macedo</p>
 			</div>
 		</div>
 	</footer>
+
+
+	<!--STUDENTS MODAL-->
+	<div class="modal fade" id="students" role="dialog">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header text-center">
+					<h3>Developers Information</h3>
+				</div>
+
+				<div class="modal-body">
+					<div class="row">
+					    <div class="col-lg-4">
+					    	<img src="<?=HOME_URL ?>/img/marioLinkedin.png">
+					    	<a href="http://goo.gl/vvodFl" class="studentsNames" target="_blank">Tiago Tavares</a>
+						</div>
+					  	
+					    <div class="col-lg-4">
+					    	<img src="<?=HOME_URL ?>/img/marioLinkedin.png">
+					    	<a href="http://goo.gl/vvodFl" class="studentsNames" target="_blank">Pedro Faria</a>
+					    </div>
+
+					    <div class="col-lg-4">
+					    	<img src="<?=HOME_URL ?>/img/marioLinkedin.png">
+					    	<a href="http://goo.gl/vvodFl" class="studentsNames" target="_blank">Mário Macedo</a>
+					    </div>
+					</div>
+
+					<div class="form-group">
+					    <div class="col-lg-12 text-right">
+					      	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					    </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	
 	<script src="<?=HOME_URL ?>/js/jquery-2.1.1.min.js"></script>
 	<script src="<?=HOME_URL ?>/js/bootstrap.js"></script>
--- project/resources/templates/navbar.php
+++ project/resources/templates/navbar.php
@@ -25,22 +25,24 @@
 <div class="modal fade" id="login" role="form">
 	<div class="modal-dialog">
 		<div class="modal-content">
-			<div class="modal-header">
-				<h2>ACCESS</h2>
+			<div class="modal-header text-center">
+				<h3>SIGN IN</h3>
 			</div>
 
 			<div class="modal-body">
 				<form class="form-horizontal" role="form">
-					<div class="form-group">
-					    <label for="username" class="col-lg-2 control-label">Username</label>
-					    <div class="col-lg-10">
+					<div class="form-group has-feedback">
+					    <!--<label for="username" class="col-lg-2 control-label">Username</label>-->
+					    <div class="inner-addon left-addon col-lg-12">
+					    	<i class="form-control-feedback glyphicon glyphicon-user"></i>
 					   		<input type="text" class="form-control" id="username" placeholder="Username">
 						</div>
 					</div>
 					  	
-					<div class="form-group">
-					   	<label for="password" class="col-lg-2 control-label">Password</label>
-					    <div class="col-lg-10">
+					<div class="form-group has-feedback has-feedback-left">
+					   	<!--<label for="password" class="col-lg-2 control-label">Password</label>-->
+					    <div class="inner-addon left-addon col-lg-12">
+					    	<i class="form-control-feedback glyphicon glyphicon-lock"></i>
 					    	<input type="password" class="form-control" id="password" placeholder="Password">
 					    </div>
 					</div>
@@ -48,8 +50,8 @@
 					<div class="form-group">
 					    <div class="col-lg-12 text-right">
 					      	<button type="submit" class="btn btn-info">Sign in</button>
-					      	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#register">Register</button>
-					      	<button type="button" class="btn btn-default">Cancel</button>
+					      	<button type="button" class="btn btn-primary" data-dismiss="modal" data-toggle="modal" data-target="#register">Register</button>
+					      	<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
 					    </div>
 					</div>
 				</form>
@@ -63,30 +65,32 @@
 <div class="modal fade" id="register" role="form">
 	<div class="modal-dialog">
 		<div class="modal-content">
-			<div class="modal-header">
-				<h2>REGISTER</h2>
+			<div class="modal-header text-center">
+				<h3>REGISTER</h3>
 			</div>
 
 			<div class="modal-body">
 				<form class="form-horizontal" role="form">
-					<div class="form-group">
-					    <label for="username" class="col-lg-2 control-label">Username</label>
-					    <div class="col-lg-3">
+					<div class="form-group has-feedback">
+					    <!--<label for="username" class="col-lg-2 control-label">Username</label>-->
+					    <div class="inner-addon left-addon col-lg-12">
 					   		<input type="text" class="form-control" id="username" placeholder="Username">
+					    	<i class="form-control-feedback glyphicon glyphicon-user"></i>
 						</div>
 					</div>
 					  	
-					<div class="form-group">
-					   	<label for="password" class="col-lg-2 control-label">Password</label>
-					    <div class="col-lg-3">
+					<div class="form-group has-feedback">
+					   	<!--<label for="password" class="col-lg-2 control-label">Password</label>-->
+					    <div class="inner-addon left-addon col-lg-12">
 					    	<input type="password" class="form-control" id="password" placeholder="Password">
+					    	<i class="form-control-feedback glyphicon glyphicon-lock"></i>
 					    </div>
 					</div>
 					
 					<div class="form-group">
-					    <div class="col-lg-2">
+					    <div class="col-lg-12 text-right">
 					      	<button type="submit" class="btn btn-info">Register</button>
-					      	<button type="button" class="btn btn-default">Cancel</button>
+					      	<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
 					    </div>
 					</div>
 			</div>
