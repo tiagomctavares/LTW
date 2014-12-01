457776906240a7c29181b0bac599b6c4fb864d70
Mário Macedo <andr.macedo1@gmail.com>
2014-12-01 13:01:41 +0000

    CSS new Poll changed and some changes on homepage

--- project/public_html/js/script.js
+++ project/public_html/js/script.js
@@ -12,7 +12,7 @@ $(document).ready(function($){
 		}
 	});
 
-	//on scolling, show/animate timeline blocks when enter the viewport
+	//on scrolling, show/animate timeline blocks when enter the viewport
 	$(window).on('scroll', function(){
 		$timeline_block.each(function(){
 			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
