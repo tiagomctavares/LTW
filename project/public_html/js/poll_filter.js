b84115355f7956b7c8647d05df8363f1b6283e7e
Tiago Tavares <tiagomctavares@gmail.com>
2014-12-05 22:33:19 +0000

    Small JS

--- project/public_html/js/poll_filter.js
+++ project/public_html/js/poll_filter.js
@@ -3,7 +3,7 @@
 $(function () {
 	var path = document.location.host + document.location.pathname;
 	var myApp;
-
+	console.log(path);
 	myApp = myApp || (function () {
 	    var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h1>Processing...</h1></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div></div></div>');
 	    return {
@@ -70,3 +70,57 @@ $(function () {
 	}
 });
 
+
+$(function() {
+	var path = document.location.host + document.location.pathname;
+	var myApp;
+
+	myApp = myApp || (function () {
+	    var pleaseWaitDiv = $('<div class="modal hide" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-header"><h1>Processing...</h1></div><div class="modal-body"><div class="progress progress-striped active"><div class="bar" style="width: 100%;"></div></div></div></div>');
+	    return {
+	        showPleaseWait: function() {
+	            pleaseWaitDiv.modal();
+	        },
+	        hidePleaseWait: function () {
+	            pleaseWaitDiv.modal('hide');
+	        },
+	    };
+	})();
+
+	$('.filters_timeline li').on('click', function() {
+		
+		if($(this).hasClass('active'))
+			return;
+		
+		//Change Classes
+		$(this).parent().find('.active').removeClass('active');
+		$(this).addClass('active');
+
+		var values = getSendData();
+		console.log(values);
+	});
+	
+
+	function getSendData() {
+		var values = [];
+		$('.filters_timeline li.active').each(function() {
+			values.push($(this).attr('id'));
+		});
+		return values;
+	}
+
+	function placeData(data) {
+		var thumb_item = $('.masonry_item').last().clone();
+		var thumb = $('#masonry_container').empty();
+
+		$.each(data, function() {
+			console.log(this);
+			if(this.image != '')
+				this.image = path + this.image;
+
+			thumb_item.find('img.pollImage').attr('src', this.image);
+			
+			thumb.append(thumb_item);
+    	});
+	}
+});
