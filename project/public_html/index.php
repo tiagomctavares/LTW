858011a007b6858d5140f9a3100aaddbeda565e3
Tiago Tavares <tiagomctavares@gmail.com>
2014-12-06 00:05:47 +0000

    Final version v1.0

--- project/public_html/index.php
+++ project/public_html/index.php
@@ -1,12 +1,4 @@
 <?php
-    # This is the first version of the structure DEPRECATED!
-
-    # This is the first version of the structure DEPRECATED!
-
-    # This is the first version of the structure DEPRECATED!
-
-    # This is the first version of the structure DEPRECATED!
-
     # MORE INFO IN README.md section FLOW
     require_once realpath(dirname(__FILE__) . "/../resources/config.php");
     require_once VIEWS_PATH.'/pages.php';
